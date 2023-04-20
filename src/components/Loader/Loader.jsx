16edb30bf8651a0bff2682e723e88a4ea574974f
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-20 22:13:38 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,13 +2,15 @@ import { RotatingLines } from 'react-loader-spinner';
 import { LoaderWrapper } from './Loader.styled';
 
 const Loader = () => (
-  <LoaderWrapper><RotatingLines
-  strokeColor="#307848"
-  strokeWidth="5"
-  animationDuration="0.75"
-  width="96"
-  visible={true}
-/></LoaderWrapper>
+  <LoaderWrapper>
+    <RotatingLines
+      strokeColor=" #b19cd9"
+      strokeWidth="5"
+      animationDuration="0.75"
+      width="46"
+      visible={true}
+    />
+  </LoaderWrapper>
 );
 
 export default Loader;
