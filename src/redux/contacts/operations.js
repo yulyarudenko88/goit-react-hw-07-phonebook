ebf3fce7bb12903d2fe245768eb05085ef4476ea
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-20 19:59:47 +0300

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,54 +1,40 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
+axios.defaults.baseURL = "";
 
-export const fetchTasks = createAsyncThunk(
-  "tasks/fetchAll",
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("/tasks");
+      const response = await axios.get("/contacts");
       return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-export const addTask = createAsyncThunk(
-  "tasks/addTask",
-  async (text, thunkAPI) => {
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async ({name, number}, thunkAPI) => {
     try {
-      const response = await axios.post("/tasks", { text });
+      const response = await axios.post("/tasks", { name, number });
       return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
 
-export const deleteTask = createAsyncThunk(
-  "tasks/deleteTask",
-  async (taskId, thunkAPI) => {
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`/tasks/${taskId}`);
+      const response = await axios.delete(`/tasks/${contactId}`);
       return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-
-export const toggleCompleted = createAsyncThunk(
-  "tasks/toggleCompleted",
-  async (task, thunkAPI) => {
-    try {
-      const response = await axios.put(`/tasks/${task.id}`, {
-        completed: !task.completed,
-      });
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
