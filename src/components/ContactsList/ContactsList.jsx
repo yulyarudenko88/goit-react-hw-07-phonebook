b705b152c829cac8d5cec01763a9f10a0a31dc09
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-20 21:13:21 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,26 +1,26 @@
 import { FiUser } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { getContacts } from 'redux/contacts/selectors';
-import { getFilter } from 'redux/filter/selectors';
-import { deleteContact } from 'redux/contacts/contactsSlice';
+import { selectVisibleContacts } from 'redux/contacts/selectors';
+// import { getFilter } from 'redux/filter/selectors';
+import { deleteContact } from 'redux/contacts/operations';
 
 import { Contact } from './ContactsList.styled';
 
 export const ContactsList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const { filter } = useSelector(getFilter);
+  const contacts = useSelector(selectVisibleContacts);
+  // const { filter } = useSelector(getFilter);
 
   const handleDeleteContact = contactId => dispatch(deleteContact(contactId));
 
-  const visibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  // const visibleContacts = contacts.filter(contact =>
+  //   contact.name.toLowerCase().includes(filter.toLowerCase())
+  // );
 
   return (
     <ul>
-      {visibleContacts.map(({ id, name, number }) => (
+      {contacts.map(({ id, name, number }) => (
         <Contact key={id}>
           <FiUser />
           {name}: {number}
