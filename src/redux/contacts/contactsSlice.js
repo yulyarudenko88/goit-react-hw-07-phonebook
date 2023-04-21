7e4af4b89a852a0590b646ae5bd304e69ef1d408
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-21 18:52:45 +0300

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -37,30 +37,12 @@ const contactsSlice = createSlice({
     [addContact.rejected]: handleRejected,
     
     [deleteContact.pending]: handlePending,
-    [deleteContact.fulfilled](state, action) {
+    [deleteContact.fulfilled](state, action) {      
       state.isLoading = false;
-      state.error = null;      
-      return state.items.filter(contact => contact.id !== action.payload);
+      state.error = null;  
+      const index = state.items.findIndex(contact => contact.id === action.payload.id);
+      state.items.splice(index, 1); 
     },
-  // reducers: {
-  //   addContact: {
-  //     reducer(state, action) {
-  //       state.push(action.payload);
-  //     },
-  //     prepare(name, number) {
-  //       return {
-  //         payload: {
-  //           id: nanoid(),
-  //           name,
-  //           number,
-  //         },
-  //       };
-  //     },
-  //   },
-
-  //   deleteContact(state, action) {
-  //     return state.filter(contact => contact.id !== action.payload);
-  //   },
   },
 });
 
