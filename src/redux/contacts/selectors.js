949d5affd4d4f47235b1f0ac262fb3cf2d5b44c5
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-21 18:51:11 +0300

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -6,18 +6,16 @@ export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 
 export const selectError = state => state.contacts.error;
-// const visibleContacts = contacts.filter(contact =>
-//   contact.name.toLowerCase().includes(filter.toLowerCase())
-// );
 
 export const selectVisibleContacts = createSelector(
   [selectContacts, selectFilter],
-  (contacts, {filter}) =>
-    {console.log(filter)
-      if (contacts===[]) {
-        return
-      }
-      return contacts.filter(contact =>
+  (contacts, { filter }) => {
+    // {console.log(filter)
+    if (contacts === []) {
+      return;
+    }
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
-    )}
+    );
+  }
 );
