c57687dc6800d602f78e3451fed2a34f9ec3593b
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2023-04-21 18:52:27 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,8 +1,7 @@
 import { FiUser } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 
-import {  selectVisibleContacts } from 'redux/contacts/selectors';
-// import { selectFilter } from 'redux/filter/selectors';
+import { selectVisibleContacts } from 'redux/contacts/selectors';
 import { deleteContact } from 'redux/contacts/operations';
 
 import { Contact } from './ContactsList.styled';
@@ -10,14 +9,8 @@ import { Contact } from './ContactsList.styled';
 export const ContactsList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectVisibleContacts);
-  // const { filter } = useSelector(selectFilter);
-  // console.log(contacts)
 
   const handleDeleteContact = contactId => dispatch(deleteContact(contactId));
-
-  // const visibleContacts = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(filter.toLowerCase())
-  // );
 
   return (
     <ul>
